555bc13a51a55d5831babd514065e61bf36ec1b7
Kuiliang Zhang <z1219202167@outlook.com>
2024-03-02 11:41:11 -0800

    feat: integrate python

--- next.config.js
+++ next.config.js
@@ -4,7 +4,19 @@
  */
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/py-api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
+};
 
-export default config;
+export default nextConfig;
