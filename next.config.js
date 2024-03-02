3b8b36ac09c1a9d1c9f4f65f84f2b9743c868188
Kuiliang Zhang <z1219202167@outlook.com>
2024-03-02 10:58:12 -0800

    fix: py-api

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/py-api/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -13,6 +13,7 @@ export const postRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
+
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
