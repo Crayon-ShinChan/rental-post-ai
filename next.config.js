e0e8d7afaf95fec40290085e57c1777a3e3526ed
Kuiliang Zhang <z1219202167@outlook.com>
2024-03-02 12:32:23 -0800

    fix: add next-api

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/py-api/:path*",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/py-api/:path*"
-            : "https://rental-post-ai.vercel.app/py-api/",
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
       },
     ];
   },
